053c7924cb217af96925c8b04c616d978a87777a
martin <mbogelund@gmail.com>
2024-12-10 00:45:21 +0100

    Added stubs for Dec05 and Dec06.

---
+++ 2024/Dec05/Part_1.py
@@ -0,0 +1,12 @@
+# AoC 2024
+# Dec05
+# Part 1
+
+import re
+
+# Import today's data
+data = [l.strip() for l in open("Input/example.txt", "rt")]
+#data = [l.strip() for l in open("Input/input.txt", "rt")]
+print(data)
+print("----------------")
+
