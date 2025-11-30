b3310a9a0d04fb1b26aa0526515d528c5dbbdb88
martin <mbogelund@gmail.com>
2025-11-30 15:37:43 +0100

    Added template for AoC2025

---
+++ 2025/Dec02/Part_1.py
@@ -0,0 +1,38 @@
+# AoC 2025
+# Dec 1
+# Part 1
+import datetime
+import re
+import os
+
+start_time = datetime.datetime.now()
+
+# Import today's data
+basename = os.path.basename(__file__)
+dirname = os.path.dirname(__file__)
+print(dirname)
+print(basename)
+#data = [l.strip() for l in open("Input/example.txt", "rt")]
+data = [l.strip() for l in open(dirname + "/Input/input.txt", "rt")]
+print(data)
+print("----------------")
+
+
+solution = None # Assign value of solution
+
+################
+#..............#
+################
+
+solution = "Pending..."
+print("#--------------------#")
+print("Solution: ", solution)
+print("#--------------------#")
+print()
+
+# Timer
+print("############# TIMER ##############")
+end_time = datetime.datetime.now()
+print("Start time: ", start_time)
+print("End time: ", end_time)
+print("Time used: ", end_time-start_time)
