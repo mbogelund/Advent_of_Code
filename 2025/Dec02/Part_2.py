6dbfbb998630c080f03e5362c76bcc839e871a36
martin <mbogelund@gmail.com>
2025-12-03 22:35:13 +0100

    Solution to December 2nd part 2.

--- 2025/Dec02/Part_2.py
+++ 2025/Dec02/Part_2.py
@@ -12,19 +12,51 @@ basename = os.path.basename(__file__)
 dirname = os.path.dirname(__file__)
 print(dirname)
 print(basename)
-#data = [l.strip() for l in open("Input/example.txt", "rt")]
+#data = [l.strip() for l in open(dirname + "/Input/example.txt", "rt")]
 data = [l.strip() for l in open(dirname + "/Input/input.txt", "rt")]
 print(data)
 print("----------------")
 
+def find_repeated_patterns(id: int):
+    id_text = str(id)
+    id_length = len(id_text)
+    for pattern_length in range(1, id_length // 2 + 1):
+        if id_length % pattern_length == 0:
+            pattern = id_text[:pattern_length]
+            #print(pattern)
+            match_indices = [match.start() for match in re.finditer(pattern, id_text)]
+            if len(match_indices) * pattern_length == id_length:
+                return id
+    return 0
 
-solution = None # Assign value of solution
+interval_list = data[0].split(",")
+#print(interval_list)
+
+sum_bogus_ids = 0
+for interval in interval_list:
+    for check_id in range(int(interval.split("-")[0]), int(interval.split("-")[1]) + 1):
+        #print(check_id)
+        bogus_id = find_repeated_patterns(check_id)
+        sum_bogus_ids = sum_bogus_ids + bogus_id
+
+#print(sum_bogus_ids)
+solution = sum_bogus_ids
+
+
+#x = find_repeated_patterns(123123)
+#print(x)
+#x = find_repeated_patterns(123123123)
+#print(x)
+#x = find_repeated_patterns(1231231234)
+#print(x)
+
+#solution = None # Assign value of solution
 
 ################
 #..............#
 ################
 
-solution = "Pending..."
+#solution = "Pending..."
 print("#--------------------#")
 print("Solution: ", solution)
 print("#--------------------#")
